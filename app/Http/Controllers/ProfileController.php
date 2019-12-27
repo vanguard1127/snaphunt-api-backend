e4335660d7c13c3c4176f39a8f495a2c0fc10754
farazirfan47 <farazirfan47@gmail.com>
2019-12-27 18:19:18 +0500

    writen profile api

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function getProfile(Request $request){
+        try {
+            $data = $request->all();
+            if(isset($data['id'])){
+                $userId = $data['id'];
+            }else{
+                $userId = $this->getAuthenticatedUser()['uuid'];
+            }
+            $user = User::where("uuid", $userId)->first();
+            if($user){
+                $response = [
+                    "uuid" => $user['uuid'],
+                    "username" => $user["username"],
+                    "full_name" => $user['first_name']." ".$user["last_name"]
+                ];
+                return $this->sendData($response);
+            }
+            return $this->errorArray();
+        } catch(ValidationException $ex){
+            return $this->validationError($ex);
+        }catch (\Exception $ex) {
+            return $this->errorArray($ex->getMessage());
+        }
+    }
+}
