f0be32cb14a9d8adfa7fd23d67d3f23c55a176ae
farazirfan47 <farazirfan47@gmail.com>
2020-01-15 22:26:48 +0500

    worked on comments

--- app/Traits/ProfileTrait.php
+++ app/Traits/ProfileTrait.php
@@ -9,15 +9,22 @@ trait ProfileTrait{
             $owner = $challenge->owner;
             $resp[] = [
                 "owner_name" => $owner["first_name"]." ".$owner["last_name"],
-                "claps" => 93,
                 "avatar" => $this->getFullURL($owner["avatar"]),
                 "thumb" => $this->getFullURL($challenge["thumb"]),
                 "media" => $this->getFullURL($challenge["media"]),
                 "desc" => $challenge["description"],
                 "post_type" => $challenge["post_type"],
-                "claps" => $challenge->claps->count(),
+                "claps" => $this->getClapCount($challenge->claps),
                 "uuid" => $challenge["uuid"]
             ];
+        }
+        return $resp;
+    }
+
+    public function getClapCount($claps){
+        $resp = [];
+        foreach($claps as $clap){
+            $resp[$clap["user_id"]] = $claps->count();
         }
         return $resp;
     }
