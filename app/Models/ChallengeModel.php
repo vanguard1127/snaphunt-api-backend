3fe4f7ad7b10d938042a0cdb800703ff85d8d132
farazirfan47 <farazirfan47@gmail.com>
2020-03-31 13:46:28 +0500

    worked on new changes

--- app/Models/ChallengeModel.php
+++ app/Models/ChallengeModel.php
@@ -47,13 +47,23 @@ class ChallengeModel extends Model
         // "media" => "required"
     ];
 
+    public static function getCategory($category, $type){
+        if($category == "null" && $type == "season1"){
+            return 17;
+        }else if ($category == "null" && $type != "season1"){
+            return null;
+        }else{
+            return $category;
+        }
+    }
+
     public static function createChallenge($data, $userId){
         return static::create(
             [
                 "post_type" => $data["post_type"],
                 "owner_id" => isset($data["owner_id"]) ? $data["owner_id"] : $userId,
                 "description" => ($data["description"] != "" ? $data["description"] : null),
-                "category" => $data["category"] != "null" ? $data["category"] : null,
+                "category" => self::getCategory($data["category"], $data["type"]),
                 "privacy" => $data["privacy"],
                 "media" => $data["media"],
                 "is_draft" => $data["is_draft"],
--- config/general.php
+++ config/general.php
@@ -17,8 +17,30 @@
             14 => "Food",
             15 => "Animals",
             16 => "Pranks"
+        ],
+        "categories_admin" => [
+            1 => "Most popular",
+            2 => "Top Savage",
+            3 => "Pay it Forward",
+            4 => "Travel",
+            5 => "Fails",
+            6 => "Extreme Sports",
+            7 => "Funny",
+            8 => "Unique",
+            9 => "Social Change",
+            10 => "Skateboarding",
+            11 => "Team Challenges",
+            12 => "Random",
+            13 => "Gaming",
+            14 => "Food",
+            15 => "Animals",
+            16 => "Pranks",
+            17 => "Season1"
         ]
     ]
+
+    // admin categories
+    // season1 = 17
 
     // challenge status
     // 1 = active
